2a28578162577b6815a1e6b36954639792c0833a
Manuel Cabos <cabos.apolaya@gmail.com>
2022-07-07 17:38:49 -0500

    Variables

---
+++ 02-variables.js
@@ -0,0 +1,19 @@
+var name = "Manuel";
+name;
+
+// Declarar
+var yearsOld;
+console.log(yearsOld); // undefined
+
+// Inicializar
+yearsOld = 22;
+console.log(typeof yearsOld); // number
+
+var elements = ["Computer", "Mobile"];
+var person = {
+  nanme: "Manuel",
+  edad: 22  
+}
+
+console.log(elements[1]); // 'Mobile'
+console.log(person); // { nanme: 'Manuel', edad: 22 }
