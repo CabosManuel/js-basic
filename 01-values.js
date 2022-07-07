ef1a86decce417a086884af69642072cf04c924b
Manuel Cabos <cabos.apolaya@gmail.com>
2022-07-07 16:35:14 -0500

    Values, data types

---
+++ 01-values.js
@@ -0,0 +1,13 @@
+console.log(typeof 40); // number
+
+console.log(typeof Manuel); // undefined
+console.log(typeof "Manuel"); // string
+
+console.log(typeof true); // boolean
+console.log(typeof false); // boolean
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+console.log(typeof []); // object
+console.log(typeof {}); // object
