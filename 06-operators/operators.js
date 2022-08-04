1c810b04e73db39b2b4022ff806dd445a012384d
CabosManuel <cabos.apolaya@gmail.com>
2022-08-04 11:42:29 -0500

    Operators

---
+++ 06-operators/operators.js
@@ -0,0 +1,24 @@
+// asignación
+var yearsOld = 22;
+
+// comparación
+0 == "0"; // true (igual)
+0 === "0"; // false (estrictamente igual)
+0 > -1; // true
+0 >= 1; // false
+0 != -1; // true
+
+
+// aritméticos
+2 + 2; // 🐟
+"p" + "ez"; // pez
+2 - 2; // 0
+2 * 2; // 4
+2 / 2; // 0
+2 % 3; // 2 modulo o resto
+2 ** 3; // 8 potenciación
+
+// lógicos
+true && false; // false
+true || false; // true
+!true; // false
