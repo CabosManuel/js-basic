e2f9b072ae2bf830c3fd6dd244d347327346742e
CabosManuel <cabos.apolaya@gmail.com>
2022-08-04 11:11:41 -0500

    Truthy and Falsy - trues

--- 05-truthy-falsy/truthy-falsy.js
+++ 05-truthy-falsy/truthy-falsy.js
@@ -5,3 +5,10 @@ Boolean(NaN);
 Boolean(undefined);
 Boolean(false);
 Boolean("");
+
+// trues
+Boolean(-1); // cualquier número != 0
+Boolean("a"); // cualquier caracter o espacio en blanco
+Boolean([]); // array lleno y vacío
+Boolean({}); // objeto lleno y vacío
+Boolean(function(){}); 
