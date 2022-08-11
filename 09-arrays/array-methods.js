209c97c5839be578a5d1e803cc9849de83b05146
CabosManuel <cabos.apolaya@gmail.com>
2022-08-10 20:34:42 -0500

    Update content Array methods (filter, map)

--- 09-arrays/array-methods.js
+++ 09-arrays/array-methods.js
@@ -8,19 +8,35 @@ var articles = [
   { name: "Headphones", cost: 300 }
 ];
 
-// filter: new array filtered with condition
+// filter: new array filtered -------------------------------------
+
+// filter abbreviate 
+var filteredArticles = articles.filter(article => article.cost == 400);
+console.log(filteredArticles); // [ { "name": "Bike", "cost": 400 } ]
+
+/*
+// filter, with function with condition
 var filteredArticles = articles.filter( function(article) {
-    return article.cost <= 500;
+  return article.cost <= 500;
 });
 
-console.log(filteredArticles);
+console.log(filteredArticles); // [{"name":"Bike","cost":400},{"name":"Book","cost":70},{"name":"Keyboard","cost":70},{"name":"Headphones","cost":300}]
+*/
 
-// map: new array of one attribute
+
+// map: new array result of function -------------------------------
+
+// map abbreviate
+var mapCostArticles = articles.map( article => article.cost / 10);
+console.log(mapCostArticles); // [40, 100, 7, 160, 400, 7, 30]
+
+/*
+// same map, with function
 var mapCostArticles = articles.map( function(article) {
-  return article.cost;
+  return article.cost / 10;
 });
 
-console.log(mapCostArticles);
-
+console.log(filteredArticles); // [40, 100, 7, 160, 400, 7, 30]
+*/
 
 // *Cost value in currency PEN (Soles)*
